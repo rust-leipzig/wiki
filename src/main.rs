7731f711162f69ec635311585d8e43f8b408c13c
Sascha Grunert <Sascha.Grunert@rohde-schwarz.com>
2017-01-20 10:04:48 +0100

    Changed internal structure to be more modular #12

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,14 @@
 #![deny(missing_docs)]
 
 extern crate markdown;
-
 extern crate getopts;
-use getopts::Options;
-use getopts::Matches;
-
 extern crate glob;
-use glob::glob;
 
+pub mod processing;
+
+use getopts::{Options, Matches};
+use processing::Processing;
 use std::env;
-use std::path::Path;
-
-use std::str;
-use std::str::FromStr;
-use std::fs::File;
-use std::io::prelude::*;
-
-#[derive(Default)]
-struct MdFiles {
-    md_file_paths: Vec<String>
-}
 
 #[derive(Default)]
 struct ArgOptions {
@@ -49,69 +37,9 @@ impl ArgOptions {
     }
 }
 
-impl MdFiles {
-    /// Add an absolute path of an markdown file to the struct
-    /// absolute path = working directory + inputparameter (relative path in the project directory)
-    /*fn add_new_md_file(&mut self, rel_path: String) {
-        let pwd = std::env::current_dir().unwrap();
-
-        let mut path = std::path::PathBuf::new();
-        path.push(pwd.to_str().unwrap());
-        path.push(rel_path);
-
-        self.md_file_paths.push(path.to_str().unwrap().to_string());
-    }*/
-
-    /// Reads all markdown files recursively from a given directory
-    fn get_markdown_from_dir(&mut self, md_dir: &str) {
-        self.md_file_paths.clear();
-        let md_path = Path::new(&md_dir).join("**").join("*.md");
-        for entry in glob(&md_path.to_str().unwrap()).expect("Failed to read glob pattern.") {
-            match entry {
-                Ok(path) => self.md_file_paths.push(String::from_str(path.to_str().unwrap()).unwrap()),
-                Err(e) => panic!(e.to_string()),
-            }
-        }
-    }
-
-    /// print absolute path of all added md files
-    fn print_all_md_paths(&self) {
-        for file in self.md_file_paths.iter() {
-            println!("{:?}", file);
-        }
-
-    }
-
-    /// read content of all files and convert it to html
-    fn read_content_from_md_file(&self) {
-        for md_file in self.md_file_paths.iter() {
-            println!("parse file: {}", md_file);
-
-            /// Error handling is not necessary here because github issue #1 "Collect all markdown files from a certain directory" should ensure that the opened file definitive exist.
-            /// But it is better to have error handling :)
-            let res = File::open(md_file);
-            match res {
-                Ok(mut file) => {
-
-                    let mut buffer = Vec::new();
-                    file.read_to_end(&mut buffer).unwrap();
-                    let md_str = str::from_utf8(&buffer).unwrap();
-                    println!("{}", markdown::to_html(md_str));
-
-                }
-                Err(e) => {
-                    println!("{}: {}", md_file, e);
-                }
-            }
-
-        }
-    }
-}
-
 fn main() {
-    let mut md = MdFiles::default();
+    // Parse the given arguments
     let mut argopts = ArgOptions::default();
-
     argopts.parse_args();
 
     let mut md_dir = String::new();
@@ -126,7 +54,9 @@ fn main() {
         }
     }
 
-    md.get_markdown_from_dir(&md_dir);
-    md.print_all_md_paths();
-    md.read_content_from_md_file();
+    // Do first processing steps
+    let mut processing = Processing::default();
+    processing.read_from_directory(&md_dir);
+    processing.list_current_paths();
+    processing.read_content_from_current_paths(); 
 }
