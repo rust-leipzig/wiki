43e534f72d5e8e4a22c9b24ef204f5244f4ebfa5
Sebastian Spiess <Sebastian.Spiess@rohde-schwarz.com>
2017-01-22 23:41:19 +0100

    [Removed] cli.yml [Changed] clap argument parsing from yaml parsing to static code

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,28 @@ extern crate clap;
 
 pub mod processing;
 
-use clap::App;
+use clap::Arg;
 use processing::Processing;
 
 static ARG_INPUT_DIRECTORY: &'static str = "INPUT";
 static ARG_OUTPUT_DIRECTORY: &'static str = "output-directory";
-static DEFAULT_HTML_DIR: &'static str = "./out";
+static DEFAULT_HTML_DIR: &'static str = "output";
 
 fn main() {
     // Parse the given arguments
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let matches = app_from_crate!()
+        .arg(Arg::from_usage("-o --output-directory=[PATH] 'The directory where the HTML output is generated.'"))
+        .arg(Arg::from_usage("<INPUT>                      'The directory containing the markdown files to use.'"))
+        .get_matches();
 
     let md_dir = matches.value_of(ARG_INPUT_DIRECTORY).unwrap();
     let html_dir = matches.value_of(ARG_OUTPUT_DIRECTORY).unwrap_or(DEFAULT_HTML_DIR);
+    // This can be deleted when html_dir is used further
+    println!("Output path: {}", html_dir);
 
     // Do first processing steps
     let mut processing = Processing::default();
     processing.read_from_directory(&md_dir);
     processing.list_current_paths();
-    processing.read_content_from_current_paths(); 
+    processing.read_content_from_current_paths();
 }
