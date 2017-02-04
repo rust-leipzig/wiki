e144fafe52bcbf6a49f96eda5b0907fec1d5a37a
Sebastian Spiess <Sebastian.Spiess@rohde-schwarz.com>
2017-02-04 17:11:01 +0100

    [Improved] error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 //! # wiki
-//!
 #![deny(missing_docs)]
 
 extern crate markdown;
@@ -10,16 +9,25 @@ extern crate clap;
 extern crate log;
 extern crate mowl;
 
+#[macro_use]
+pub mod error;
 pub mod processing;
 
 use clap::Arg;
 use processing::Processing;
+use error::{WikiError, ErrorType};
+use std::process::exit;
 
 static ARG_INPUT_DIRECTORY: &'static str = "INPUT";
 static ARG_OUTPUT_DIRECTORY: &'static str = "output-directory";
 static DEFAULT_HTML_DIR: &'static str = "output";
 
-fn main() {
+fn error_and_exit(error: WikiError) {
+    error!("{}", error);
+    exit(1);
+}
+
+fn run(mut retval: WikiError) -> WikiError {
     // Parse the given arguments
     let matches = app_from_crate!()
         .arg(Arg::from_usage("-o --output-directory=[PATH] 'The directory where the HTML output is generated.'"))
@@ -30,14 +38,30 @@ fn main() {
     let html_dir = matches.value_of(ARG_OUTPUT_DIRECTORY).unwrap_or(DEFAULT_HTML_DIR);
 
     // Init logger crate
-    mowl::init().unwrap();
+    match mowl::init() {
+        Ok(_) => debug!("Mowl logging initiated."),
+        Err(_) => {
+            retval.code = ErrorType::InitFailure;
+            return retval;
+        },
+    }
 
     // This can be deleted when html_dir is used further
     debug!("Output path: {}", html_dir);
 
     // Do first processing steps
     let mut processing = Processing::default();
-    processing.read_from_directory(&md_dir);
+
+    return_if_not_ok!(processing.read_from_directory(&md_dir));
     processing.list_current_paths();
-    processing.read_content_from_current_paths();
+    return_if_not_ok!(processing.read_content_from_current_paths());
+    return retval;
+}
+
+fn main() {
+    let mut retval = WikiError::default();
+    retval = run(retval);
+    if retval.code != ErrorType::Ok {
+        error_and_exit(retval);
+    }
 }
--- src/processing.rs
+++ src/processing.rs
@@ -3,9 +3,11 @@ use glob::glob;
 use markdown::to_html;
 
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::io::prelude::*;
 use std::str;
+
+use error::{WikiError, ErrorType};
 
 #[derive(Default)]
 /// Global processing structure
@@ -14,27 +16,41 @@ pub struct Processing {
     paths: Vec<PathBuf>,
 }
 
+/// The global result type for wiki
+pub struct WikiResult {
+    /// The `WikiError` error type for the current result
+    pub error: WikiError
+}
+
 impl Processing {
     /// Reads all markdown files recursively from a given directory
     /// Clears the current available paths
-    pub fn read_from_directory(&mut self, directory: &str) {
+    pub fn read_from_directory(&mut self, directory: &str) -> WikiError {
         /// Remove all paths
         self.paths.clear();
 
         /// Gather new content
         let md_path = PathBuf::from(&directory).join("**").join("*.md");
-
-        /// TODO: Error handling
-        for entry in glob(md_path.to_str().unwrap()).expect("Failed to read glob pattern.") {
-            match entry {
-                Ok(path) => self.paths.push(path),
-                Err(e) => error!("{}", e.to_string()),
-            }
+        if Path::new(&directory).is_dir() == false {
+            return WikiError::new(ErrorType::PathNotExisting, &format!("The path '{}' does not exist", directory));
         }
+
+        match glob(md_path.to_str().unwrap()) {
+            Ok(entries) => {
+                for entry in entries {
+                    match entry {
+                        Ok(path) => self.paths.push(path),
+                        Err(e) => return WikiError::new(ErrorType::PathNotReadable, &e.to_string()),
+                    }
+                }
+            },
+            Err(e) => return WikiError::new(ErrorType::FileNotReadable, e.msg),
+        }
+        WikiError::new(ErrorType::Ok, "")
     }
 
     /// Read the content of all files and convert it to HTML
-    pub fn read_content_from_current_paths(&self) {
+    pub fn read_content_from_current_paths(&self) -> WikiError {
         // Iterate over all available paths
         for file in &self.paths {
             info!("Parsing file: {}", file.display());
@@ -43,13 +59,15 @@ impl Processing {
             match File::open(file) {
                 Ok(mut file) => {
                     let mut buffer = String::new();
-                    // TODO: Error handling
-                    file.read_to_string(&mut buffer).unwrap();
-                    debug!("{}", to_html(&buffer));
+                    match file.read_to_string(&mut buffer) {
+                        Ok(_) => debug!("{}", to_html(&buffer)),
+                        Err(e) => return WikiError::new(ErrorType::BufferStringifyFailed, &e.to_string()),
+                    }
                 }
-                Err(e) => error!("{}: {}", file.display(), e),
+                Err(e) => return WikiError::new(ErrorType::FileNotReadable , &e.to_string()),
             }
         }
+        WikiError::new(ErrorType::Ok, "")
     }
 
     /// Print absolute path of all added md files
