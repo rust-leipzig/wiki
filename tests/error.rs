af0ab2ef2e921278d493d1e25c540ff155f5895b
Sebastian Spieß <spiess.sebastian@web.de>
2017-05-03 23:06:59 +0200

    [Increased] test coverage (#52)

---
+++ tests/error.rs
@@ -0,0 +1,15 @@
+extern crate wikilib;
+
+use wikilib::error::*;
+use std::error::Error as StdError;
+use std::io;
+
+static ERROR_STR: &str = "Something went wrong here.";
+
+#[test]
+fn io_error_to_wiki_error() {
+    let io_error = io::Error::new(io::ErrorKind::NotFound, ERROR_STR);
+    let wiki_error: Error = io_error.into();
+    assert_eq!(wiki_error.description(), ERROR_STR.to_string());
+}
+
