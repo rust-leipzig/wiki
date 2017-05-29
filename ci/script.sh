d0b32f51f97524c5d140c1a1e904a97917f06a07
Sebastian Spiess <spiess.sebastian@web.de>
2017-05-29 20:30:11 +0200

    [Changed] travis-cargo to cargo

--- ci/script.sh
+++ ci/script.sh
@@ -1,11 +1,11 @@
 export CARGO_TARGET_DIR=`pwd`/target
 
 if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    RUST_BACKTRACE=1 travis-cargo build --features "clippy"
+    RUST_BACKTRACE=1 cargo build --features "clippy"
 else
-    RUST_BACKTRACE=1 travis-cargo build
+    RUST_BACKTRACE=1 cargo build
 fi
 
-RUST_BACKTRACE=1 travis-cargo test
+RUST_BACKTRACE=1 cargo test
 
 cargo doc --no-deps
