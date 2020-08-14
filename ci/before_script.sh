15f2c3a659d34ce554a7ff2ae1f9eb20c8f8c222
Stefan Müller zzeroo <co@zzeroo.com>
2020-08-14 18:56:49 +0200

    Install clippy only on nightly

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -4,3 +4,7 @@ export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/Library/Python/2.7/bin:$PATH
 
 cargo install cargo-kcov
+
+if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]] then
+  rustup component add clippy --toolchain=nightly || cargo install --git https://github.com/rust-lang/rust-clippy/ --force clippy
+fi
